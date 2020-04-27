b0d3004c97270f84b03fe0c68390183d11e37466
Kristina <codingkris@yahoo.com>
2020-04-27 14:56:22 +0300

    Scroll monitor added

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+var LOAD_NUM = 4;
+var watcher;
+
 new Vue({
   el: "#app",
   data: {
@@ -6,7 +9,8 @@ new Vue({
     cart: [],
     search: "cat",
     lastSearch: "",
-    loading: false
+    loading: false,
+		results: []
   },
   methods: {
     addToCart: function (product) {
@@ -41,15 +45,26 @@ new Vue({
     },
     onSubmit: function () {
       this.products = [];
+      this.results = [];
 			this.loading = true;
       var path = "/search?q=".concat(this.search);
       this.$http.get(path)
         .then(function (response) {
-          this.products = response.body;
+          this.results = response.body;
           this.lastSearch = this.search;
+          this.appendResults();
           this.loading = false;
         });
-    }
+    },
+    appendResults: function() {
+			if(this.products.length < this.results.length) {
+				var toAppend = this.results.slice(
+					this.products.length,
+					LOAD_NUM + this.products.length
+				);
+				this.products = this.products.concat(toAppend);
+			}
+		}
   },
   filters: {
     currency: function (price) {
@@ -58,5 +73,16 @@ new Vue({
   },
   created: function() {
     this.onSubmit();
-  }
+  },
+  updated: function() {
+		var sensor = document.querySelector("#product-list-bottom");
+		watcher = scrollMonitor.create(sensor);
+		watcher.enterViewport(this.appendResults);
+	},
+	beforeUpdate: function() {
+		if (watcher) {
+			watcher.destroy();
+			watcher = null;
+		}
+	}
 });
