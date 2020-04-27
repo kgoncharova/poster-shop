9712faf5057c38f92ef9edf83da49ae2bda9c838
Kristina <codingkris@yahoo.com>
2020-04-27 10:47:43 +0300

    Added quantity to cart items

--- public/script.js
+++ public/script.js
@@ -3,16 +3,21 @@ new Vue({
   data: {
     total: 0,
     products: [
-			{ title: "Product 1", id: 1, price: 9.99 },
-			{ title: "Product 2", id: 2, price: 9.99 },
-			{ title: "Product 3", id: 3, price: 9.99 }
+      { title: "Product 1", id: 1, price: 9.99 },
+      { title: "Product 2", id: 2, price: 9.99 },
+      { title: "Product 3", id: 3, price: 9.99 }
     ],
     cart: []
   },
   methods: {
     addToCart: function (product) {
-			this.total += product.price;
-			this.cart.push(product);
-		}
-	}
+      this.total += product.price;
+      this.cart.push({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        qty: 1
+      });
+    }
+  }
 });
